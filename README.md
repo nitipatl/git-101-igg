fea8218a425c47f2a9d5077703bb86027e963adb
Nitipat L <iamsvz@gmail.com>
2020-09-16 13:39:54 +0700

    [Added] example js

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# git-104-igg
+# การใช้ Markdown คร่าวๆ : https://bit.ly/2FHTfBm
 
-## AAAA
+# ตัวอย่าง : https://bit.ly/3hzI4bs
+
+# สร้างแบบเร็วๆ : https://bit.ly/3iCm5ld
+
+# Sheet (กรอกทุกคน) : https://bit.ly/3c0Okrz
+
+Markdown
---
+++ js/example.js
@@ -0,0 +1,18 @@
+let name = "John"
+name = "JOKE"
+let test = "Hello"
+name = `${test} B`
+
+
+let arr = ['apple', 'banana', 'kiwi']
+
+
+let person = {
+    name: 'JOKE',
+    age: 15,
+    gender: {
+        name: 'male'
+    },
+    arr
+}
+console.log(person)
