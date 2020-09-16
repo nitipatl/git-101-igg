de90145d0e11070e9e35884852ab8d0e9feb5049
Nitipat L <iamsvz@gmail.com>
2020-09-16 14:05:30 +0700

    [Added] function file
    description of functioin and arrow function

---
+++ js/function.js
@@ -0,0 +1,17 @@
+// function add(a, b) {
+//     return a + b
+// }
+
+// function mul(a, b) {
+//     return a * b
+// }
+
+// function searchCustomer(customerName) {
+//     // find some customer
+// }
+
+const add = (a,b) => {
+    return a + b
+} 
+
+console.log(add(2, 3))
